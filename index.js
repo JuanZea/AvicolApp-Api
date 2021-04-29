c59b726d381d2ee5936a569fd7ff741da1ddf0a7
LAPTOP-4HF2DFTC\Andres <andresguerra0625@gmail.com>
2021-04-28 20:52:55 -0500

    feat: Sequelize added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express');
+//Import .env configurations
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
 
+const express = require('express');
 const app = express();
+const sequelize = require('./database/db');
 
 // Routes
 app.get('/', (req, res) => {
@@ -9,4 +14,11 @@ app.get('/', (req, res) => {
 
 app.listen(8000, () => {
   console.log('Server on port 8000')
+
+  // Connect with database
+  sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+  }).catch(error => {
+    console.error('Unable to connect to the database:', error);
+  })
 });
