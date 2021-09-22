ac33287a3ac78e4d93690b623712d3e9a594d3ec
Alejandro Yarce <53787660+DonYarce@users.noreply.github.com>
2021-09-21 23:47:14 -0500

    feat: add lot controller

--- app/controllers/LotController.js
+++ app/controllers/LotController.js
@@ -1,14 +1,22 @@
-const { Lot } = require('../../database');
+const { Lot, Barn } = require('../../database');
 
 module.exports = {
-  async index(req, res) {
-    let lots = await Lot.findAll();
 
-    let response = {
-      status: 200,
-      data: lots,
+  async index(req, res) {
+
+    const response = {status: 200};
+    const barn = await Barn.findOne({where: {id: req.headers.barn_id}, include: [{model: Lot}]});
+
+    if (!barn) {
+      response.message = "Barn not found";
+      response.status = 404;
+    } else if (barn.settlement_id.toString() !== req.headers.settlement_id) {
+      response.message = "The barn belongs to another settlement";
+      response.status = 401;
+    } else {
+      response.data = await barn.getLots();
     }
 
-    res.json(response);
+      res.status(response.status).json(response);
   }
 }
