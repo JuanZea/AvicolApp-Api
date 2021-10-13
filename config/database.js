c0230216aa10f62f1b8fe05a7e2d747c31493dda
Valeria Granada Rodas <55161707+vale0722@users.noreply.github.com>
2021-10-13 18:23:15 -0500

    No hagan estoy en casa v.2

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ if (process.env.NODE_ENV !== 'production') {
 
 module.exports = {
   database: process.env.DB_DATABASE || "development",
-  username: 'admin',
-  password: 'password',
-  dialect: 'mysql',
+  username: "admin",
+  password: "password",
+  dialect: "mysql",
   options: {
-    dialect:'mysql',
-    host: avicolapp.cmu2tr1w5xst.us-east-1.rds.amazonaws.com,
+    dialect:"mysql",
+    host: "avicolapp.cmu2tr1w5xst.us-east-1.rds.amazonaws.com",
     port: "3306",
     define: {
       underscored: true,
